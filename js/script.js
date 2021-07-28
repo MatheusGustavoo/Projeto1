780a4536b824e2979160b84542b8970ceb03c715
Matheus Gustavo <matheus.pedra@etec.sp.gov.br>
2021-07-28 12:42:37 -0300

    slide

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 import dropdown from "./dropdown.js";
 import animarScroll from "./anima-scroll.js";
+import SlideNav from "./slide.js";
+
+const slide = new SlideNav(".slide-ul", ".slide");
+slide.init();
+slide.addArrow(".prev", ".next");
+slide.addControl(".custom-controls");
 dropdown();
 animarScroll();
